6987219ac17fd1bbf2e17bf6ab6be302f83fcd73
Cody Lewis <hello@codymlewis.com>
2024-07-10 15:48:50 +1000

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -14,11 +14,6 @@
       virtualenv venv
       source venv/bin/activate
       pip install -r requirements.txt
-      if [[ $(lspci | grep 'NVIDIA') ]]; then
-        pip install "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
-      else
-        pip install jax
-      fi
     else
       source venv/bin/activate
     fi
