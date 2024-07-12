3462f940408ebcae48ca98556022d6fb99812cf4
Cody Lewis <hello@codymlewis.com>
2024-07-12 14:50:34 +1000

    Added viceroy experiments script

---
+++ viceroy/src/run_experiments.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+
+repeat_experiment () {
+  for ((i = 1; i <= $2; i++)); do
+    $1 -s $i
+  done
+}
+
+for dataset in "mnist" "cifar10" "kddcup99"; do
+  for aggregator in "fedavg" "median" "krum" "foolsgold" "contra" "stddagmm" "viceroy"; do
+    for compressor in "none" "autoencoder" "fedzip" "fedmax" "fedprox" "topk"; do
+      # for adversary_type in "none" "labelflipper" "scaling_labelflipper" "backdoor" "scaling_backdoor" "freerider" "onoff_labelflipper" "onoff_backdoor" "onoff_freerider" "goodmouther" "badmouther"; do
+      for adversary_type in "none" "labelflipper" "backdoor" "freerider" "onoff_labelflipper" "onoff_freerider" "goodmouther" "badmouther"; do
+        for percent_adversaries in "0.1" "0.3" "0.5"; do
+          repeat_experiment "python main.py --clients 100 --rounds 500 --epochs 10 --dataset $dataset --aggregator $aggregator --compressor $compressor --adversary-type $adversary_type --percent_adversaries $percent_adversaries" 5
+        done
+      done
+    done
+  done
+done
